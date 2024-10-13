554e6cd6d30b9332369f7a92298b114f680157af
karthik-j7799 <kart33539@gmail.com>
2024-10-13 23:51:57 +0530

    updated url

--- src/baseurl.js
+++ src/baseurl.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://43.204.92.123:4002/health_empire_api', 
-  baseURL: 'http://localhost:4002/health_empire_api', 
+  baseURL: 'https://fitnessback.onrender.com/health_empire_api', 
 
   headers: {
     'Content-Type': 'application/json', 
